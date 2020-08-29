1d4523c70807c7c5e3ad813b01f3039e0497a7f2
dbokser <dbokser@gmail.com>
2020-08-29 18:14:14 -0500

    Added key beats and better visibility for control inputs

--- Nature Lad/Assets/CameraFacing.cs
+++ Nature Lad/Assets/CameraFacing.cs
@@ -7,16 +7,30 @@ namespace NatureLad
     public class CameraFacing : MonoBehaviour
     {
         private Transform _cam;
+        [SerializeField]
+        private bool _lockOnYPlane = true;
+        [SerializeField]
+        private bool _reverseLook = false;
+        [SerializeField]
+        private bool _reverseUp = false;
 
         // Update is called once per frame
         void Update()
         {
             Vector3 aimPos = Camera.main.transform.position - transform.position;
-            aimPos.y = 0f;
+            if(_lockOnYPlane)
+            {
+                aimPos.y = 0f;
+            }            
 
             aimPos.Normalize();
 
-            transform.rotation = Quaternion.LookRotation(aimPos, Vector3.up);
+            if(_reverseLook)
+            {
+                aimPos *= -1f;
+            }
+
+            transform.rotation = Quaternion.LookRotation(aimPos, _reverseUp ? Vector3.down : Vector3.up);
         }
     }
 }
