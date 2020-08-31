10f62f27867a20cff7d9bd505316d0bedae1dfd9
dbokser <dbokser@gmail.com>
2020-08-31 00:22:08 -0500

    adjusted positions of crowd animals. Added escape to quit

--- Nature Lad/Assets/GameMaster.cs
+++ Nature Lad/Assets/GameMaster.cs
@@ -39,7 +39,10 @@ namespace NatureLad
         // Update is called once per frame
         void Update()
         {
-            
+            if (Input.GetKey("escape"))
+            {
+                Application.Quit();
+            }
         }
 
         public void EnterOwlStage()
